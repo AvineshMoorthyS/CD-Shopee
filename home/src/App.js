40541d4149058a52628527bb956e6dcb01a97b6e
AvineshMoorthyS <avineshsit@gmail.com>
2022-08-28 14:11:11 +0530

    CD Shopee

---
+++ home/src/App.js
@@ -0,0 +1,36 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Navbar from 'react-bootstrap/Navbar';
+import {LinkContainer} from 'react-router-bootstrap'
+import Container from 'react-bootstrap/Container';
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="d-flex flex-column site-container">
+      <header>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+             <LinkContainer to="/">
+              <Navbar.Brand>CD Shopee</Navbar.Brand>
+             </LinkContainer>
+          </Container>
+        </Navbar>
+      </header>
+      <main>
+        <Container>
+        <Routes>
+          <Route path="/" element={<HomeScreen/>}/>
+          <Route path="/product/:slug" element={<ProductScreen/>} />
+        </Routes>
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center">All rights reserved</div>
+      </footer>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
